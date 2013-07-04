6dc9248e1421c9185e422a850ce4ceccf2fe1e89
Krzysztof Rutka <krzysztof.rutka@gmail.com>
2013-07-04 13:38:46 +0200

    Initial commit; knife cookbook create

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'erlang'
+maintainer       'Krzysztof Rutka'
+maintainer_email 'krzysztof.rutka@gmail.com'
+license          'Apache 2.0'
+description      'Installs Erlang'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
